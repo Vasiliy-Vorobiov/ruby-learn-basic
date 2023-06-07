5976f79ec2b6883ec20e404df548d3542326e4fe
Vasiliy Vorobiov <135667979+Vasiliy-Vorobiov@users.noreply.github.com>
2023-06-07 23:48:35 +0300

    add comments to some files

--- ch_11_blocks_2.rb
+++ ch_11_blocks_2.rb
@@ -1,3 +1,6 @@
+# Дан массив %w[cat dog tiger]. Нужно вернуть массив, состоящий только из 
+# элементов, где встречается символ t.
+
 arr = %w[cat dog tiger]
 
 arr2 = arr.each_with_object([]) do |elem, l_arr|
